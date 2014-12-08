849ac8c025ff83b6fbf169d5361b99ca03f22b9c
leomilrib <leo@leomilrib.com>
2014-12-08 18:40:20 -0200

    Add test environment

--- environments.rb
+++ environments.rb
@@ -1,6 +1,11 @@
 configure :development do
- set :database, 'sqlite3:dev.db'
+ set :database, 'sqlite3:db/dev.db'
  set :show_exceptions, true
+end
+
+configure :test do
+ set :database, 'sqlite3:db/test.db'
+ set :show_exceptions, false
 end
 
 configure :production do
